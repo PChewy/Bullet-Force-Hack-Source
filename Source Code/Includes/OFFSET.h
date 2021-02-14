3681ca434da5af94d6644127cef5adb2a2c83f8e
Lry <65631161+PChewy@users.noreply.github.com>
2021-02-13 23:16:52 -0500

    TeleKill

---
+++ Source Code/Includes/OFFSET.h
@@ -0,0 +1,9 @@
+#ifndef ANDROID_MOD_MENU_GLOBAL_H
+#define ANDROID_MOD_MENU_GLOBAL_H
+
+static uintptr_t get_transform1 = 0x180C9C4; //Component_get_transform
+static uintptr_t set_position_Injected1 = 0x7189D0; //Transform_set_position_Injected
+static uintptr_t get_position_Injected1 = 0x71891C; //Transform_get_position_Injected
+static uintptr_t PlayerScript1 = 0x958464; //PlayerScript_UpdateFast
+
+#endif
